54b4ecec9676039d6691c34f9056590aa81a86c7
Pamela <pamela_bhopal@hotmail.co.uk>
2021-04-12 15:18:18 +0100

    javascript boiler plate created

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,22 @@
-console.log("it works");
+const getFromLocalStorage = () => {
+  const localStorageData = JSON.parse(localStorage.getItem("cities"));
+
+  if (localStorageData === null) {
+    return [];
+  } else {
+    return localStorageData
+  }
+}
+
+const onSubmit = (Event) => {
+  Event.preventDefault();
+
+  const cityName = $("city-input").val();
+  const cities = getFromLocalStorage();
+
+  cities.push(cityName);
+
+  localStorage.setItem("cities", cities);
+}
+
+$(#search-by-city-form").on("submit", onSubmit)
