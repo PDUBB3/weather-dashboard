37d6c45bb4b1cc41373e8ed6f60ddc341e44f4a0
Pamela <pamela_bhopal@hotmail.co.uk>
2021-04-12 15:33:10 +0100

    add event listener for search bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,6 @@
-const getFromLocalStorage = () => {
-  const localStorageData = JSON.parse(localStorage.getItem("cities"));
+const onSubmit = (event) => {
+  event.preventDefault();
+  console.log("onsubmit");
+};
 
-  if (localStorageData === null) {
-    return [];
-  } else {
-    return localStorageData
-  }
-}
-
-const onSubmit = (Event) => {
-  Event.preventDefault();
-
-  const cityName = $("city-input").val();
-  const cities = getFromLocalStorage();
-
-  cities.push(cityName);
-
-  localStorage.setItem("cities", cities);
-}
-
-$(#search-by-city-form").on("submit", onSubmit)
+$("#search-by-city-form").on("submit", onSubmit);
